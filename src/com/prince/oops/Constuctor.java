7366928b3bdef841e59e979891c3b47d6dbb5159
prince-007-max <arulprince2004@gmail.com>
2024-09-24 15:03:51 +0530

    modified the main.java

---
+++ src/com/prince/oops/Constuctor.java
@@ -0,0 +1,29 @@
+package com.prince.oops;
+
+public class Constuctor {
+    double l;
+    double h;
+    double w;
+
+
+    Constuctor () {
+        this.h = 0;
+        this.l = 0;
+        this.w = 10;
+
+    }
+
+    Constuctor (double a, double b){
+        this.h = a;
+        this.l = b;
+    }
+
+    Constuctor (Constuctor old){
+        this.h = old.h;
+        this.l = old.l;
+        this.w = old.w;
+
+    }
+
+
+}
--- src/com/prince/oops/Main.java
+++ src/com/prince/oops/Main.java
@@ -7,5 +7,13 @@ public class Main {
         Human prince = new Human(19,"john",12000,false);
         System.out.println(prince.name);
         System.out.println(Human.population);
+
+        Constuctor size = new Constuctor();
+        Constuctor size1 = new Constuctor(12,14);
+        Constuctor size2 = new Constuctor(size);
+
+        System.out.println(size1.h);
+
+        System.out.println(size2.w);
     }
 }
